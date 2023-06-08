04bc0cfc2b92570cc993e5a754f3782f693ac7a9
Bruce Barbet Coburn <bcoburn1@uwyo.edu>
2023-06-08 11:20:35 -0600

    Adding template code to help debug the output shapes at each layer (can make more programmatic)

--- dev_code/playground/lightning_test/lightning_test.py
+++ dev_code/playground/lightning_test/lightning_test.py
@@ -14,10 +14,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+# Import cnn model for torchvision
+from torchvision.models import resnet18
+
 import pytorch_lightning as pl
 
 from PIL import Image
 
+from torchsummary import summary
 
 class CNN(pl.LightningModule):
     """
@@ -30,21 +34,100 @@ class CNN(pl.LightningModule):
         super(CNN, self).__init__()
         # 1 input image channel, 10 output channels, 3x3 square convolution
         # kernel
+
+        # Convolution Layer 1
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        # Convolution Layer 2
         self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=3, stride=1, padding=0)
-        self.fc1 = nn.Linear(20 * 5 * 5, 100)  # 5x5 image dimension (from conv2)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        # Flatten Layer
+        self.flatten = nn.Flatten()
+
+        # Fully Connected Layer 1
+        self.FULLY_CONNECTED_INPUTS = self.flatten(torch.ones(1, 20, 5, 5)).shape[1]
+        self.fc1 = nn.Linear(self.FULLY_CONNECTED_INPUTS, 100)
+        self.relu3 = nn.ReLU()
+
+        # Output Layer
         self.fc2 = nn.Linear(100, 10)  # 10 classes
 
-        self.dummy_input = torch.ones(1, 1, 28, 28)
+        self.dummy_input = torch.ones(64, 1, 28, 28)
+
+        self._print_layer_output_shape()
+
+    def nice_shape_print(self, layer_name, module_names, tensors):
+
+        border_length = len(layer_name) + 4
+        top_border = '*' * border_length
+        bottom_border = '-' * border_length
+
+        print(top_border)
+        print(f'* {layer_name} *')
+        print(bottom_border)
+        for idx, name in enumerate(module_names):
+            print(name, 'output shape:', tensors[idx].shape)
+
+    def calculate_layer(self, first_input, first_layer, *args):
+        outputs = []
+        output = first_layer(first_input)
+        outputs.append(output)
+
+        for arg in args:
+            output = arg(output)
+            outputs.append(output)
+
+        return outputs
+
+    def _print_layer_output_shape(self):
+
+        # Dummy input
+        self.nice_shape_print('Dummy Input', ['Input shape'], [self.dummy_input])
+
+        # Convolution Layer 1
+        modules = ['conv1', 'relu1', 'maxpool1']
+        outputs = self.calculate_layer(self.dummy_input, self.conv1, self.relu1, self.maxpool1)
+        self.nice_shape_print('Convolution Layer 1', modules, outputs)
+
+        # Convolution Layer 2
+        modules = ['conv2', 'relu2', 'maxpool2']
+        outputs = self.calculate_layer(outputs[-1], self.conv2, self.relu2, self.maxpool2)
+        self.nice_shape_print('Convolution Layer 2', modules, outputs)
+
+        # Flatten Layer
+        modules = ['flatten']
+        outputs = [self.flatten(outputs[-1])]
+
+        # Fully Connected Layer 1
+        modules = ['fc1', 'relu3']
+        outputs = self.calculate_layer(outputs[-1], self.fc1, self.relu3)
+        self.nice_shape_print('Fully Connected Layer 1', modules, outputs)
+
+        # Output Layer
+        modules = ['fc2']
+        outputs = [self.fc2(outputs[-1])]
+        self.nice_shape_print('Output Layer', modules, outputs)
 
     def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)  # 2x2 window
-        x = x.view(-1, self.num_flat_features(x))  # flatten
-        x = F.relu(self.fc1(x))
+        # Convolution Layer 1
+        x = self.maxpool1(self.relu1(self.conv1(x)))
+
+        # Convolution Layer 2
+        x = self.maxpool2(self.relu2(self.conv2(x)))
+
+        # Flatten Layer
+        x = self.flatten(x)
+
+        # Fully Connected Layer 1
+        x = self.relu3(self.fc1(x))
+
+        # Output Layer
         x = self.fc2(x)
+
         return x
 
     def num_flat_features(self, x):
